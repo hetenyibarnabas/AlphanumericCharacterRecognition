e3b37e6eba5129d24f555193cc845e992a23ccc4
Hetényi Barnabás <231098106+hetenyibarnabas@users.noreply.github.com>
2026-09-02 13:55:56 +0200

    Add TorchSharp cpu support

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TorchSharp;
+
+var tensor = torch.tensor(new float[]
+{
+    1, 2, 3,
+    4, 5, 6
+});
+
+tensor = tensor.reshape(2, 3);
+
+Console.WriteLine("TorchSharp works!");
+Console.WriteLine(tensor[0, 0].item<float>());
